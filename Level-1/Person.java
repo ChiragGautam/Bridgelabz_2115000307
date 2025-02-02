ff05fb08096c8b563488427a4622b4b759d1fed8
Chirag Gautam <chirag.gautam-cs21@gla.ac.in>
2025-02-02 22:37:24 +0530

    Add codes for 31-01-2025

---
+++ Level-1/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    public void Display() {
+        System.out.println("Person name: " + name);
+        System.out.println("Person age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Arun", 22);
+        Person p2 = new Person(p1);
+        p1.Display();
+        p2.Display();
+    }
+}
